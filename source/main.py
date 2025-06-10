5c008e39588cf0f2226c8acf05d3b046ace58b79
Bob The Builder <151051388+RIZKI756@users.noreply.github.com>
2025-06-10 12:06:35 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,8 +1,1 @@
-from app.store import Magasin
 
-# Initialisation
-magasin = Magasin("MonSupermarche")
-magasin.charger_produits("data/products.json")
-
-# Test
-print("Produits chargés :", magasin.produits)
