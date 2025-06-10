05eccc6b0f4a6017001482cf09051a2464f993f9
MaelFH <164360128+MaelFH@users.noreply.github.com>
2025-06-10 16:03:55 +0200

    Update project_model.py
    ajouts de class

--- app1_config_magasin/model/project_model.py
+++ app1_config_magasin/model/project_model.py
@@ -1,1 +1,20 @@
+class ProjectModel:
+def init(self):
+self.project_name = ""
+self.author = ""
+self.address = ""
+self.image_path = ""
+self.grid_size = 50
+self.grid_origin = (0, 0)
 
+def set_project_info(self, name, author, address):
+    self.project_name = name
+    self.author = author
+    self.address = address
+
+def set_image_path(self, path):
+    self.image_path = path
+
+def set_grid_parameters(self, size, origin_x, origin_y):
+    self.grid_size = size
+    self.grid_origin = (origin_x, origin_y)
