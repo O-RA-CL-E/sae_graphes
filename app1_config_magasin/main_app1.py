7718dd867ffba77056e792b0a19729df506d5090
ORΔCLΣ <154529081+O-RA-CL-E@users.noreply.github.com>
2025-06-10 15:51:12 +0200

    Update main_app1.py and reorganization of the first app

--- app1_config_magasin/main_app1.py
+++ app1_config_magasin/main_app1.py
@@ -1,1 +1,27 @@
+from PyQt5.QtWidgets import QApplication, QInputDialog, QMessageBox
+import sys
 
+from model.project_model import ProjectModel
+from view.project_view import ProjectView
+from controller.project_controller import ProjectController
+
+def ask_password():
+    correct_password = "admin123"
+    password, ok = QInputDialog.getText(None, "Accès sécurisé", "Mot de passe :", echo=QInputDialog.Password)
+    if not ok or password != correct_password:
+        QMessageBox.critical(None, "Erreur", "Mot de passe incorrect. Fermeture.")
+    sys.exit()
+
+def main():
+    app = QApplication(sys.argv)
+    ask_password()
+
+model = ProjectModel()
+view = ProjectView()
+controller = ProjectController(model, view)
+
+view.show()
+sys.exit(app.exec_())
+
+if name == "main":
+    main()
