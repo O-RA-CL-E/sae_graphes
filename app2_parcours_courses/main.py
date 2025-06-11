716c739cf68def5005b015a847e8014d9814509b
Bob The Builder <151051388+RIZKI756@users.noreply.github.com>
2025-06-11 11:05:07 +0200

    Update main.py

--- app2_parcours_courses/main.py
+++ app2_parcours_courses/main.py
@@ -1,6 +1,1 @@
-from controller.core import Controller
 
-if __name__ == "__main__":
-    print("=== Application Parcours Courses ===")
-    mon_controleur = Controller()
-    mon_controleur.demarrer()
