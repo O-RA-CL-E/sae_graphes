c424fb23881502e63a207c4e51e651d3b1782712
Bob The Builder <151051388+RIZKI756@users.noreply.github.com>
2025-06-11 10:08:08 +0200

    Update main_app2.py

--- app2_parcours_courses/main_app2.py
+++ app2_parcours_courses/main_app2.py
@@ -1,1 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from controller.controller_app2 import ControllerApp2
 
+if name == "main":
+app = QApplication(sys.argv)
+controller = ControllerApp2()
+controller.run()
+sys.exit(app.exec_())
