fc0a6cca64c1577725b7c1d0e9c8f4ec8a26d075
Bob The Builder <151051388+RIZKI756@users.noreply.github.com>
2025-06-11 11:05:30 +0200

    Update magasin.py

--- app2_parcours_courses/model/magasin.py
+++ app2_parcours_courses/model/magasin.py
@@ -1,19 +1,1 @@
-import json
-import random
 
-class Magasin:
-    def __init__(self):
-        self.produits = []
-        self.plan = ""
-    
-    def charger_projet(self, chemin_projet):
-        try:
-            with open(f"{chemin_projet}/produits.json") as f:
-                self.produits = json.load(f)
-            self.plan = f"{chemin_projet}/plan.png"
-            return True
-        except:
-            return False
-    
-    def generer_liste_aleatoire(self, n=5):
-        return random.sample(self.produits, n)
