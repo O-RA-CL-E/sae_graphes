60db5c6839e8d06b9561abdccb6c3e31ae0b65fb
Bob The Builder <151051388+RIZKI756@users.noreply.github.com>
2025-06-11 10:59:15 +0200

    Update core.py

--- app2_parcours_courses/controller/core.py
+++ app2_parcours_courses/controller/core.py
@@ -1,1 +1,16 @@
+from model.magasin import Magasin
+from view.interface import Interface
 
+class Controller:
+    def __init__(self):
+        self.modele = Magasin()
+        self.vue = Interface(self)
+    
+    def demarrer(self):
+        self.vue.afficher_menu_principal()
+    
+    def charger_projet(self, chemin):
+        return self.modele.charger_projet(chemin)
+    
+    def generer_liste(self):
+        return self.modele.generer_liste_aleatoire()
